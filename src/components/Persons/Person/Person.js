fe241d7bba4c9e0eaf4f6a034bd78867b1df7add
Josep Escrivá <josep.escrivamorato@manomano.com>
2020-11-19 08:54:52 +0100

    Using React.Fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,7 +6,7 @@ class Person extends Component {
     
     render (){
         return (
-        <Aux>
+        <React.Fragment>
             <p key="1" onClick={this.props.click} >I'm {this.props.name} and I have {this.props.age} years old! </p>
             <p key="2">{this.props.children}</p>
             <input 
@@ -15,8 +15,7 @@ class Person extends Component {
                 onChange={this.props.changed}  
                 value={this.props.name}
             />
-        </Aux>
-            
+        </React.Fragment>
         );
     }
 };
